eeb2483502a95754ac3f37e042e4e90643e792c9
张凯 <56509647@qq.com>
2020-04-22 19:58:13 +0800

    ZK封装Admins和Schedule两个实体模型

---
+++ ApiCore/Model/Schedule.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model
+{
+    //车辆预定记录表
+    public class Schedule
+    {
+        public int ScheduleID { get; set; }
+        public int uid { get; set; }
+        public int OrdersID { get; set; }
+        public bool Hitch { get; set; } //故障
+
+    }
+}
