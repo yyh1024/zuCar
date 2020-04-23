ba9fe955e91177a2cf23f6faa2b499ef84b5e47d
张凯 <56509647@qq.com>
2020-04-23 21:55:32 +0800

    21:55 ZK更改CarDal

--- ApiCore/DAL/CarDal.cs
+++ ApiCore/DAL/CarDal.cs
@@ -17,7 +17,7 @@ namespace DAL
         }
 
 
-     
+
         //车辆挂靠
         public int AddVa(Va v)
         {
@@ -27,27 +27,28 @@ namespace DAL
 
         //个人订单查询
         public List<Orders> OrdersShow(int UsersId)
-            {
-                    string Str=$"select * from CarType c join CarInfo i on c.CarTypeID=i.cid join CarBrand b on i.bid=b.CarBrandID join Orders o on i.CarInfoID=o.CarInfoid join Users u on o.uid=u.Uid where o.uid={UsersId}";
+        {
+            string Str = $"select * from CarType c join CarInfo i on c.CarTypeID=i.cid join CarBrand b on i.bid=b.CarBrandID join Orders o on i.CarInfoID=o.CarInfoid join Users u on o.uid=u.Uid where o.uid={UsersId}";
             return DBHelper.GetToList<Orders>(Str);
         }
 
         //个人挂靠信息
         public List<Va> VaShow(int UsersId)
         {
-          string Str=$"select * from CarType c join Va v on c.CarTypeID=v.cid join CarBrand b on v.bid=b.CarBrandID where v.uid={UsersId}";
+            string Str = $"select * from CarType c join Va v on c.CarTypeID=v.cid join CarBrand b on v.bid=b.CarBrandID where v.uid={UsersId}";
+            return DBHelper.GetToList<Va>(Str);
         }
 
-       //个人信息认证，   根据他的姓名和身份证是否为空来判断有没有个人认证
-       public list<UserInfo> UserInfoShow(int UsersId)
-       {
-          string Str=$"select * from Users u1 join UserInfo u2 on u1.Uid=u2.uid where u1.Uid={UsersId}";
+        //个人信息认证,根据他的姓名和身份证是否为空来判断有没有个人认证
+        public list<UserInfo> UserInfoShow(int UsersId)
+        {
+            string Str = $"select * from Users u1 join UserInfo u2 on u1.Uid=u2.uid where u1.Uid={UsersId}";
             return DBHelper.GetToList<UserInfo>(Str);
         }
 
 
 
-        
+
 
     }
 }
