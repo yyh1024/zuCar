3f73dd3ca79ee233eae7091d38911d61fca9619b
o <o@LAPTOP-PRF64I3N>
2020-04-24 11:43:46 +0800

    2020-4-24 zjl 修改api （显示。认证。挂靠）

---
+++ ApiCore/ApiCore/Controllers/zjlCar3Controller.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using DAL;
+using Model;
+using BLL;
+
+namespace ApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class zjlCar3Controller : ControllerBase
+    {
+
+        CarBll bll = new CarBll();
+        //个人信息认证显示
+        // GET: api/zjlCar3
+        [HttpGet]
+        public IEnumerable<UserInfo> Get(int  UsersId)
+        {
+            return bll.UserInfoShow(UsersId);
+        }
+
+        // GET: api/zjlCar3/5
+        [HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+
+        //个人信息认证 （用添加来做认证）
+        // POST: api/zjlCar3
+        [HttpPost]
+        public int Post(UserInfo u)
+        {
+            return bll.AddUserInfo(u);
+        }
+
+        // PUT: api/zjlCar3/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
--- ApiCore/BLL/CarBll.cs
+++ ApiCore/BLL/CarBll.cs
@@ -1,10 +1,58 @@
 ﻿using System;
 using System.Data;
 using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Text;
+using DAL;
+using Model;
 
 namespace BLL
 {
     public class CarBll
     {
+        CarDal dal = new CarDal();
+        //全部汽车显示
+        public List<CarInfo> CarShow()
+        {
+
+            return dal.CarShow();
+        }
+
+        //汽车详情
+        public CarInfo Find(int id)
+        {
+            return dal.Find(id);
+        }
+
+
+        //车辆挂靠
+        public int AddVa(Va v)
+        {
+            return dal.AddVa(v);
+        }
+
+        //个人订单查询
+        public List<Orders> OrdersShow(int UsersId)
+        {
+            return dal.OrdersShow(UsersId);
+        }
+
+        //个人挂靠信息
+        public List<Va> VaShow(int UsersId)
+        {
+            return dal.VaShow(UsersId);
+        }
+
+        //个人信息认证,根据他的姓名和身份证是否为空来判断有没有个人认证
+        public List<UserInfo> UserInfoShow(int UsersId)
+        {
+            return dal.UserInfoShow(UsersId);
+        }
+
+        //个人认证
+        public int AddUserInfo(UserInfo u)
+        {
+            return dal.AddUserInfo(u);
+        }
     }
 }
