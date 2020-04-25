d5515255e2b5b80d283460a4a99bdf679b583ff3
张凯 <56509647@qq.com>
2020-04-25 13:04:48 +0800

    13:04  更换API

---
+++ CarAPI/CarAPI/Controllers/UserController.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Model;
+using BLL;
+using DAL;
+using Microsoft.AspNetCore.Cors;
+
+namespace ApiCore.Controllers
+{
+    [EnableCors("any")] //跨域配置
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        UserBll userBll = new UserBll();
+
+        // GET: api/User
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "value1", "value2" };
+        }
+
+        // GET: api/User/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST: api/User
+        [HttpPost]
+        public int Post([FromBody] Users m)
+        {
+            MailVeriCodeClass.SendMailMessage(m.Email);
+            return userBll.AddUsers(m);
+        }
+
+        // PUT: api/User/5
+        [HttpPut("{id}")]
+        public int Put([FromBody]Users m)
+        {
+            return userBll.AlterUsers(m);
+        }
+
+        // DELETE: api/ApiWithActions/5
+        [HttpDelete("{id}")]
+        public int Delete(string id)
+        {
+            return userBll.DelUsers(id);
+        }
+
+        //public IActionResult RandomMailCode()
+        //{
+        //    return MailVeriCodeClass.CreateRandomMailCode();
+        //}
+
+    }
+}
---
+++ CarAPI/CarAPI/Controllers/zjlCarController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using BLL;
+using Model;
+using Microsoft.AspNetCore.Cors;
+
+namespace ApiCore.Controllers
+{
+    [EnableCors("any")] //跨域配置
+    [Route("api/[controller]")]
+    [ApiController]
+    public class zjlCarController : ControllerBase
+    {
+        CarBll bll = new CarBll();
+
+        //所有汽车显示
+        // GET: api/zjlCar
+        [HttpGet]
+        public IEnumerable<CarInfo> Get()
+        {
+            return bll.CarShow();
+        }
+
+
+        //汽车详情
+        // GET: api/zjlCar/5
+        [HttpGet("{id}", Name = "Get")]
+        public CarInfo Get(int id)
+        {
+            return bll.Find(id);
+        }
+
+
+
+
+        //// POST: api/zjlCar
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT: api/zjlCar/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE: api/ApiWithActions/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
